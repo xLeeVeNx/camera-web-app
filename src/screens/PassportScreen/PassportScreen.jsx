4177aefe7fcc9aa6de2016c293060488b668f1de
xLeeVeNx <berdyevwork@mail.ru>
2023-01-25 16:39:01 +0600

    fix

--- src/screens/PassportScreen/PassportScreen.jsx
+++ src/screens/PassportScreen/PassportScreen.jsx
@@ -10,8 +10,8 @@ import {useOutletContext} from 'react-router-dom';
 const videoConstraints = {
   width: 1920,
   height: 1080,
-  // facingMode: { exact: "environment" }
-  facingMode: 'user',
+  facingMode: { exact: "environment" }
+  // facingMode: 'user',
 };
 
 const docType = 'passport_main';
